25be9431777be330a613234117a2601632c390a9
lottemarava <lottemarava@gmail.com>
2021-11-28 14:10:26 +0200

    6 basic functions before tests

--- part2/product.c
+++ part2/product.c
@@ -1,5 +1,6 @@
 #include "amount_set.h"
 #include "matamikya.h"
+#include "product.h"
 
 struct product_t {
     char* product_description;
@@ -8,7 +9,6 @@ struct product_t {
     //need to implement
     MatamikyaAmountType amount_type;
     double profit;
-    product_t* next_product;
     /* double amount; no need to include in the struct single_item_in_order cause the items in the orders organized in amount set ADT,
     therefore exit build in a field for amount */
 };
@@ -20,15 +20,67 @@ struct mtmProductData_t {
  
 MtmProductData copyProductData (MtmProductData product_data)
 {
-
+    if(product_data == NULL){
+        return NULL;
+    }
+    ProductData new_data = (ProductData)malloc(sizeof(*new_data));  //malloc!!!! need to free 1
+    if(new_data == NULL){
+        return NULL;
+    }
+    new_data->price_for_quantity = ((ProductData)product_data)->price_for_quantity;
+    return (MtmProductData)new_data;
 }
 
-MtmProductData freeProductData (MtmProductData product_data)
+void freeProductData (MtmProductData product_data)
 {
-
+    free((ProductData)product_data);
 }
 
-MtmProductData compareProductData (MtmProductData product_data, const double amount)
+double getProductPrice (MtmProductData product_data, const double amount)
 {
-    
+    return (((ProductData)product_data)->price_for_quantity * amount);
+}
+
+////product////
+ASElement copyProduct (ASElement product)
+{
+    if(product == NULL){
+        return NULL;
+    }
+    Product new_product = malloc(sizeof(*new_product));   
+    if (new_product == NULL){
+        return NULL;
+    }
+    char* name_copy = malloc(strlen(((Product)product)->product_description)+1);
+    if(name_copy == NULL){
+        free(new_product);
+        return NULL;
+    }
+    strcpy (name_copy, ((Product)product)->product_description);
+    new_product->product_description = name_copy;
+    new_product->product_id = ((Product)product)->product_id;
+    new_product->amount_type = ((Product)product)->amount_type; // ככה מעתיקים enum?
+    new_product->profit = ((Product)product)->profit;
+    new_product->custom_data = copyProductData(((Product)product)->custom_data);
+    if(new_product->custom_data == NULL) { //  אם הקצאת הקסטום דאטא לא עבדה זה מה שצריך? 
+        free (name_copy);
+        free(new_product);
+        return NULL;
+    }
+    return (ASElement)new_product;
+}
+
+void freeProduct(ASElement product) 
+{
+    if (product==NULL) {
+        return NULL; //אולי להחזיר הצלחה?
+    }
+    free(((Product)product)->product_description);
+    free(((Product)product)->custom_data);
+    free ((Product)product);
+}
+
+int compareProduct (ASElement product_exist, ASElement product_to_add)
+{
+    return (((Product)product_exist)->product_id) - (((Product)product_exist)->product_id);
 }
--- part2/product.h
+++ part2/product.h
@@ -5,15 +5,16 @@
 
 //typedef for structs
 typedef struct product_t *Product;
+typedef struct mtmProductData_t *ProductData;
 
 //functions for using generic ADT amount set for restoring items in storage
 ASElement copyProduct (ASElement product);
-ASElement freeProduct (ASElement product);
-ASElement compareProduct (ASElement product_exist, ASElement product_to_add);
+void freeProduct (ASElement product);
+int compareProduct (ASElement product_exist, ASElement product_to_add);
 
 //functions for using MtmProductData for restoring more information about the product
 MtmProductData copyProductData (MtmProductData product_data);
-MtmProductData freeProductData (MtmProductData product_data);
-MtmProductData compareProductData (MtmProductData product_data, const double amount);
+void freeProductData (MtmProductData product_data);
+double getProductPrice (MtmProductData product_data, const double amount);
 
 #endif /*PRODUCT_H*/
