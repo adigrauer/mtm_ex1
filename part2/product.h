1a9b94b6d7d1a03ceacbd556f0d476a73104ee40
lottemarava <lottemarava@gmail.com>
2021-11-28 17:32:02 +0200

    after valgrind

--- part2/product.h
+++ part2/product.h
@@ -5,20 +5,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct product_t {
-    char* product_description;
-    unsigned int product_id;
-    MtmProductData custom_data;
-    //need to implement
-    MatamikyaAmountType amount_type;
-    double profit;
-    /* double amount; no need to include in the struct single_item_in_order cause the items in the orders organized in amount set ADT,
-    therefore exit build in a field for amount */
-};
-
-struct mtmProductData_t {
-    double price_for_quantity; 
-}; 
 
 //typedef for structs
 typedef struct product_t *Product;
--- part2/test_product.c
+++ part2/test_product.c
@@ -4,11 +4,10 @@
 
 int main () 
 {
-    printf ("*********Test_1***********");
-    bool check=true;
+    printf ("*********Test_1***********\n");
     Product test_product= createProduct();
     Product copy = copyProduct(test_product);
     freeProduct(copy);
     freeProduct(test_product);
-    printf ("are there any leaks?");
+    printf ("test1 finished, check for leaks");
 }
