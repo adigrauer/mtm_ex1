a6b0dcb5bd8fc92f01a17701ba64b605bb3ee772
adigrauer <adigrauer@campus.technion.ac.il>
2021-11-29 17:34:20 +0200

    functions for orders in matamikya

--- part2/matamikya.c
+++ part2/matamikya.c
@@ -3,6 +3,7 @@
 #include "matamikya.h"
 #include "orders.h"
 #include "product.h"
+#include "util.h"
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -11,6 +12,7 @@
 struct Matamikya_t{
    AmountSet storage;
    Set orders; 
+   unsigned int last_orders_id;
 };
 /////////////////////////////////////////////////
 
@@ -20,7 +22,7 @@ Matamikya matamikyaCreate()
    if(new_warehouse == NULL){
       return NULL;
    }
-   new_warehouse->storage =/*need to change the functions*/ asCreate(copyProduct, freeProduct, compareProduct); //malloc!!! need to free 2
+   new_warehouse->storage = asCreate(copyProduct, freeProduct, compareProduct); //malloc!!! need to free 2
    if(new_warehouse->storage == NULL){
       free(new_warehouse);  //free 1 in case of error
       return NULL;
@@ -30,6 +32,7 @@ Matamikya matamikyaCreate()
       asDestroy(new_warehouse->storage); //free 2 in case of error
       free(new_warehouse);  //free 1 in case of error
    }
+   new_warehouse->last_orders_id = 0;
    return new_warehouse;
 }
 
@@ -45,16 +48,58 @@ void matamikyaDestroy(Matamikya matamikya)
 
 unsigned int mtmCreateNewOrder(Matamikya matamikya)
 {
-   unsigned int new_order_id = selectIdForNewOrder(matamikya->orders);
+   if(matamikya == NULL){
+      return 0;
+   }
+   unsigned int new_order_id = matamikya->last_orders_id + 1;
    OrderInformation new_order = createNewEmptyOrder(new_order_id);
    if(new_order == NULL){
       return 0;
    }
-   if(setAdd(matamikya->orders, new_order) == );
+   if(setAdd(matamikya->orders, new_order) == SET_SUCCESS){
+      matamikya->last_orders_id = new_order_id;
+      return new_order_id;
+   }
 }
 
 MatamikyaResult mtmChangeProductAmountInOrder(Matamikya matamikya, const unsigned int order_id,
                   const unsigned int product_id, const double amount)
+{
+   /*if(cheack if the item exsit in storage){
+      return MATAMIKYA_PRODUCT_NOT_EXIST;
+   }*/
+   if(matamikya == NULL){
+      return MATAMIKYA_NULL_ARGUMENT;
+   }
+   if(checkIfOrderExistById(matamikya->orders, order_id) == false){
+      return MATAMIKYA_ORDER_NOT_EXIST;
+   }
+   AmountSet ptr_order = findSpecificOrderInOrders(matamikya->orders, order_id);
+   if(ptr_order == NULL){
+      return MATAMIKYA_OUT_OF_MEMORY;
+   }
+   if(checkIfItemExistInOrderById(ptr_order, order_id) == false){
+      return MATAMIKYA_PRODUCT_NOT_EXIST;
+   }
+   /*if(){ lottem function
+      return MATAMIKYA_INVALID_AMOUNT;
+   }*/
+   unsigned int* temp_product_id = createNewIdForItemInOrder(product_id);
+   double current_amount = 0;
+   asGetAmount(ptr_order, temp_product_id, &current_amount);
+   if(changeAmountOfItemInOrder(ptr_order, product_id, amount) == AS_INSUFFICIENT_AMOUNT || current_amount == 0){
+      asDelete(ptr_order, temp_product_id);
+      return MATAMIKYA_SUCCESS;
+   }
+   return MATAMIKYA_SUCCESS;
+}
+
+bool checkIfAmountInOrderValidForShipping(AmountSet items_in_order)
+{
+   
+}
+
+MatamikyaResult mtmShipOrder(Matamikya matamikya, const unsigned int order_id)
 {
    if(matamikya == NULL){
       return MATAMIKYA_NULL_ARGUMENT;
@@ -62,15 +107,34 @@ MatamikyaResult mtmChangeProductAmountInOrder(Matamikya matamikya, const unsigne
    if(checkIfOrderExistById(matamikya->orders, order_id) == false){
       return MATAMIKYA_ORDER_NOT_EXIST;
    }
-   AmountSet ptr_order = findSpecificOrderInOrders (matamikya->orders, order_id);
-   if(ptr_order == NULL){
+   AmountSet items_in_order = findSpecificOrderInOrders(matamikya->orders, order_id);
+   if(items_in_order == NULL){
       return MATAMIKYA_OUT_OF_MEMORY;
    }
-   if(checkIfItemExistInOrderById(ptr_order, order_id) == false){
-      return MATAMIKYA_PRODUCT_NOT_EXIST;
-   }
-   
+
+   return MATAMIKYA_INSUFFICIENT_AMOUNT;
 }
+
+MatamikyaResult mtmCancelOrder(Matamikya matamikya, const unsigned int order_id)
+{
+   if(matamikya == NULL){
+      return MATAMIKYA_NULL_ARGUMENT;
+   }
+   if(checkIfOrderExistById(matamikya->orders, order_id) == false){
+      return MATAMIKYA_ORDER_NOT_EXIST;
+   }
+   OrderInformation temp_order = createNewEmptyOrder(order_id);
+   if(temp_order == NULL){
+      return MATAMIKYA_OUT_OF_MEMORY;
+   }  
+   setRemove(matamikya->orders, temp_order);
+   return MATAMIKYA_SUCCESS;
+}
+
+
+
+
+
 
 
 
